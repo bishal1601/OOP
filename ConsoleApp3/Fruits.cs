42e8fc0511487353bb8799088665619f8322822f
Bishal <bishalshah431@gmail.com>
2025-06-26 15:55:58 +0545

    OOP

---
+++ ConsoleApp3/Fruits.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp3;
+
+public class Fruits
+{
+    public string? Name, Color;
+    public int Weight;
+
+    public void PrintName()
+    {
+        Console.WriteLine(Name);
+        Console.WriteLine(Color);
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp3;
+
+public static class Apple{
+    static void Main()
+    {
+        Fruits fruits = new Fruits();
+        fruits.Name = Console.ReadLine();
+        fruits.Color = Console.ReadLine();
+        fruits.Weight = int.Parse(Console.ReadLine());
+        fruits.PrintName();
+    }
+}
